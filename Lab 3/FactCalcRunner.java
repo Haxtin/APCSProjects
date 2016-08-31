698c62fbc6a90dd718aa322d69175983a05a5348
Haxtin <dylanveghead@gmail.com>
2016-08-31 08:45:30 -0700

    Add files via upload

---
+++ Lab 3/FactCalcRunner.java
@@ -0,0 +1,18 @@
+
+/**
+ *  
+ * @Dylan Fuhs 
+ * 
+ */
+public class FactCalcRunner
+{
+   public static void main(String [] args){
+       int n1 = 3;
+       
+       FactCalc fc = new();
+       System.out.println("Number = " +n1);
+       System.out.println("The Factorial of" + n1 "is :" + fc.factorial(n1));
+    }
+       
+       
+}
---
+++ Lab2/SimpleCalc.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class SimpleCalc here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SimpleCalc
+{
+    // instance variables - replace the example below with your own
+    public SimpleCalc(int a, int b){
+    }
+}
